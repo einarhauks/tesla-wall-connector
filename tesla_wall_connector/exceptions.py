90bf267f742fbd7ae4f48a9fa1c7c47bbe69d0ff
einarhauks <einar.hauksson@gmail.com>
2021-12-09 17:08:08 +0000

    Add handling for nan in JSON response (#3)
    * Add handling for nan in JSON response
    
    Add new exception type for decode errors with the possibility of getting the raw response

--- tesla_wall_connector/exceptions.py
+++ tesla_wall_connector/exceptions.py
@@ -5,7 +5,20 @@ class WallConnectorError(Exception):
     """Generic WallConnector exception."""
 
 
-class WallConnectorEmptyResponseError(Exception):
+class WallConnectorDecodeError(WallConnectorError):
+    """WallConnector exception thrown when decoding a response fails."""
+
+    def __init__(self, message, raw_body):
+        """Init Decode error."""
+        super().__init__(message)
+        self.raw_body = raw_body
+
+    def get_raw_body(self) -> str:
+        """Return the raw body of the failing request."""
+        return self.raw_body
+
+
+class WallConnectorEmptyResponseError(WallConnectorError):
     """WallConnector empty API response exception."""
 
 
